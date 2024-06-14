59ecedd7b792e33c2b5115a107466fdb3fbf80b5
Veia-Atua Sun <v3low1999@go.byuh.edu>
2024-06-14 08:54:51 -1000

    Update code

--- Activity-18/scripts/script.js
+++ Activity-18/scripts/script.js
@@ -4,18 +4,23 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    var password = document.getElementById('phone').value;
+
 
     var usernameValid = /^[a-zA-Z0-9]{5,}$/.test(username); // Username should be at least 5 characters long and contain only letters and numbers
     var emailValid = /^[^@]+@\w+(\.\w+)+\w$/.test(email); // Simple email pattern check
     var passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password); // Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters
+    var phone = /^[+0-9]{8,}$/.test(phone);
 
     document.getElementById('usernameFeedback').style.display = usernameValid ? 'none' : 'block';
     document.getElementById('emailFeedback').style.display = emailValid ? 'none' : 'block';
     document.getElementById('passwordFeedback').style.display = passwordValid ? 'none' : 'block';
+    document.getElementById('phoneFeedback'). style.display = phoneValid ? 'none' : 'block';
 
     document.getElementById('usernameFeedback').textContent = usernameValid ? '' : 'Username should be at least 5 characters long and contain only letters and numbers.';
     document.getElementById('emailFeedback').textContent = emailValid ? '' : 'Please enter a valid email address.';
     document.getElementById('passwordFeedback').textContent = passwordValid ? '' : 'Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters.';
+    document.getElementById('phoneFeedback'). textContent = phoneValid ? '' : '';
 
     var formValid = usernameValid && emailValid && passwordValid;
 
