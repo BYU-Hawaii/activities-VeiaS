e53af858c88fca638e17b435de9f07ce73bd22f6
Veia-Atua Sun <v3low1999@go.byuh.edu>
2024-06-14 09:16:08 -1000

    update css link

--- Activity-18/scripts/script.js
+++ Activity-18/scripts/script.js
@@ -4,7 +4,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    var password = document.getElementById('phone').value;
+    var phone = document.getElementById('phone').value;
 
 
     var usernameValid = /^[a-zA-Z0-9]{5,}$/.test(username); // Username should be at least 5 characters long and contain only letters and numbers
@@ -20,9 +20,9 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     document.getElementById('usernameFeedback').textContent = usernameValid ? '' : 'Username should be at least 5 characters long and contain only letters and numbers.';
     document.getElementById('emailFeedback').textContent = emailValid ? '' : 'Please enter a valid email address.';
     document.getElementById('passwordFeedback').textContent = passwordValid ? '' : 'Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters.';
-    document.getElementById('phoneFeedback'). textContent = phoneValid ? '' : '';
+    document.getElementById('phoneFeedback'). textContent = phoneValid ? '' : 'Please enter a valid phone number.';
 
-    var formValid = usernameValid && emailValid && passwordValid;
+    var formValid = usernameValid && emailValid && passwordValid && phoneValid;
 
     if (formValid) {
         document.getElementById('registrationFeedback').textContent = 'Your user registration was accepted!';
